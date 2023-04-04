af92364a221088309db0af362e06304ed02a72ae
Jan <janekrembikowski@gmail.com>
2023-04-04 18:02:05 +0200

    setup server + clean and format code

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
 import { useState } from "react";
-import BookCreate from './components/BookCreate';
+import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
-function App () {
-    const [books, setBooks] = useState([]);
+function App() {
+  const [books, setBooks] = useState([]);
 
-    const createBook = (title) => {
-        const updatedBooks = [
-            ...books, { 
-            id: Math.round(Math.random() * 9999),
-             title: title }
-        ]
-        setBooks(updatedBooks);
-    };
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      {
+        id: Math.round(Math.random() * 9999),
+        title: title,
+      },
+    ];
+    setBooks(updatedBooks);
+  };
 
-    const deleteBookById = (id) => {
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
-        });
-        setBooks(updatedBooks);
-    }
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
 
-    const editBookById = (id, newTitle) => {
-        const updatedBooks = books.map((book) => {
-            if (book.id === id) {
-                return { ...book, title: newTitle }
-            }
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
 
-            return book;
-        });
+      return book;
+    });
 
-        setBooks(updatedBooks);
-    };
+    setBooks(updatedBooks);
+  };
 
-    return (
-        <div className="app">
-            <h1>Reading List</h1>
-            <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
-            <BookCreate onSubmit={createBook}/>
-        </div>
-    );
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+      <BookCreate onSubmit={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,13 @@
 import BookShow from "./BookShow";
 
-    function BookList({ books, onDelete, onEdit }) {
-        const renderedBooks = books.map((book) => {
-            return <BookShow onDelete={onDelete} key={book.id}
-             book={book} onEdit={ onEdit }/>
-        });
-    
-        return (
-        <div className={"book-list"}>
-            {renderedBooks}
-        </div>
-    )
+function BookList({ books, onDelete, onEdit }) {
+  const renderedBooks = books.map((book) => {
+    return (
+      <BookShow onDelete={onDelete} key={book.id} book={book} onEdit={onEdit} />
+    );
+  });
+
+  return <div className={"book-list"}>{renderedBooks}</div>;
 }
 
 export default BookList;
